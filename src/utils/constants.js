a773b195b64fa8b6d16c6bc5cd8254b58b858558
Evgenia Kurakina <101995565+krokodila888@users.noreply.github.com>
2023-02-14 10:15:57 +0300

    PropTypes really fixed now

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,19 +14,4 @@ export const ingredientPropTypes = PropTypes.shape({
   type: PropTypes.string.isRequired,
   __v: PropTypes.number.isRequired,
   _id: PropTypes.string.isRequired,
-}).isRequired;
-
-export const currentSelectedIngredient = PropTypes.shape({
-    calories: PropTypes.number,
-    carbohydrates: PropTypes.number,
-    fat: PropTypes.number, 
-    image: PropTypes.string,
-    image_large: PropTypes.string,
-    image_mobile: PropTypes.string,
-    name: PropTypes.string,
-    price: PropTypes.number, 
-    proteins: PropTypes.number, 
-    type: PropTypes.string,
-    __v: PropTypes.number,
-    _id: PropTypes.string,
-  });
+});
