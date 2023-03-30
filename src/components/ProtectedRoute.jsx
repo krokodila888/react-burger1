f9e29d27a1433498f72caaf4ae0d37e1a424e088
Evgenia Kurakina <101995565+krokodila888@users.noreply.github.com>
2023-03-30 12:23:12 +0300

    Delete ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ loggedIn, children, url  }) => {
-
-  return loggedIn ? children : <Navigate to={url} /*replace*/ />;
-}
-
-export default ProtectedRoute;
